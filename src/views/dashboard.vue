5b1fed5737632b6c21035aa4b71f15c397fdc53d
1998YUKI0331 <dkenl0110@dongguk.edu>
2021-10-15 23:12:22 +0900

    restore earlier state

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -50,11 +50,12 @@
   </section>
 </template>
 
-<script lang='js'>
+<script lang="js">
 import pieChart from '../components/charts/examples/pieChart'
 import JQuery from 'jquery'
 import S3config from './Key.js'
 import axios from 'axios'
+import lamejs from 'lamejs'
 let $ = JQuery
 
 export default {
@@ -86,10 +87,6 @@ export default {
     pieChart
   },
   methods: {
-    init: function () {
-      let stripeScript= document.createElement('script');
-      stripeScript.setAttribute('src', 'https://js.stripe.com/v3/');
-    },
     toggleProBanner: function () {
       $('body').toggleClass('pro-banner-collapse')
     },
@@ -116,7 +113,7 @@ export default {
       })
     },
     imgClicked (script) {
-      var textarea = document.querySelector('textarea')
+      var textarea = document.querySelector('textarea#script')
       textarea.value = script
       axios.get('http://localhost:3000/')
         .then(res => {
@@ -141,7 +138,7 @@ export default {
       if (typeof MediaRecorder.isTypeSupported === 'function') {
         var options = {
           bitsPerSecond: 80000,
-          mimeType: 'audio/webm;codecs=opus' }
+          mimeType: 'video/webm;codecs=opus' }
         this.mediaRecorder = new MediaRecorder(this.localstream, options)
       } else {
         this.mediaRecorder = new MediaRecorder(this.localstream)
@@ -163,19 +160,18 @@ export default {
       })
       this.mediaRecorder.stop()
       return new Promise((resolve, reject) => {
-        this.mediaRecorder.onstop = function () {
-          const file = new Blob(this.chunks, {type: 'audio/webm'})
+        this.mediaRecorder.onstop = async function () {
+          var File = new Blob(this.chunks, {type: 'video/webm'})
+          // var file = await this.convertToMP3(tmpFile)
           this.chunks = []
-          const fileName = 'audio_' + this.sending_index + '.mp3'
-
-          W3Module.convertWebmToMP3(file).then(async (mp3Blob) => {
-            await S3.putObject({
-              Bucket: S3config.Bucket,
-              Key: fileName,
-              ACL: 'public-read',
-              Body: mp3Blob
-            }).promise()
-          })
+          const fileName = 'audio_' + this.sending_index + '.webm'
+          await S3.putObject({
+            Bucket: S3config.Bucket,
+            Key: fileName,
+            ACL: 'public-read',
+            Body: File,
+            ContentType: 'video/mpeg'
+          }).promise()
           resolve()
         }.bind(this)
       })
@@ -184,7 +180,7 @@ export default {
       if (typeof MediaRecorder.isTypeSupported === 'function') {
         var options = {
           bitsPerSecond: 80000,
-          mimeType: 'audio/webm;codecs=opus' }
+          mimeType: 'video/webm;codecs=opus' }
         this.mediaRecorder_Even = new MediaRecorder(this.localstream, options)
       } else {
         this.mediaRecorder_Even = new MediaRecorder(this.localstream)
@@ -207,14 +203,16 @@ export default {
       this.mediaRecorder_Even.stop()
       return new Promise((resolve, reject) => {
         this.mediaRecorder_Even.onstop = async function () {
-          const file = new Blob(this.chunks_Even, {type: 'audio/webm'})
+          var File = new Blob(this.chunks_Even, {type: 'video/webm'})
+          // var file = await this.convertToMP3(tmpFile)
           this.chunks_Even = []
           const fileName = 'audio_' + this.sending_index + '.webm'
           await S3.putObject({
             Bucket: S3config.Bucket,
             Key: fileName,
             ACL: 'public-read',
-            Body: file
+            Body: File,
+            ContentType: 'video/webm'
           }).promise()
           resolve()
         }.bind(this)
@@ -303,6 +301,87 @@ export default {
     ShowPopTitle () {
       const pop = document.getElementById('popPosition')
       pop.style.display = 'block'
+    },
+    convertToMP3 (blob) {
+      return new Promise((resolve, reject) => {
+        const audioContext = new AudioContext()
+        blob.arrayBuffer().then(function (arrayBuffer) {
+          // Convert array buffer into audio buffer.
+          audioContext.decodeAudioData(arrayBuffer).then(function (aBuffer) {
+            try {
+              // audio Buffer To Wav
+              const numOfChan = aBuffer.numberOfChannels
+              const btwLength = aBuffer.length * numOfChan * 2 + 44
+              const btwArrBuff = new ArrayBuffer(btwLength)
+              const btwView = new DataView(btwArrBuff)
+              const btwChnls = []
+              let btwSample
+              let btwPos = 0
+              let btwOffset = 0
+              const setUint16 = (value) => {
+                btwView.setUint16(btwPos, value, true)
+                btwPos += 2
+              }
+              const setUint32 = (value) => {
+                btwView.setUint32(btwPos, value, true)
+                btwPos += 4
+              }
+              setUint32(0x46464952) // "RIFF"
+              setUint32(btwLength - 8) // file length - 8
+              setUint32(0x45564157) // "WAVE"
+              setUint32(0x20746d66) // "fmt " chunk
+              setUint32(16) // length = 16
+              setUint16(1) // PCM (uncompressed)
+              setUint16(numOfChan)
+              setUint32(aBuffer.sampleRate)
+              setUint32(aBuffer.sampleRate * 2 * numOfChan) // avg. bytes/sec
+              setUint16(numOfChan * 2) // block-align
+              setUint16(16) // 16-bit
+              setUint32(0x61746164) // "data" - chunk
+              setUint32(btwLength - btwPos - 4) // chunk length
+              for (let btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++) {
+                btwChnls.push(aBuffer.getChannelData(btwIndex))
+              }
+              while (btwPos < btwLength) {
+                for (let btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
+                  // interleave btwChnls
+                  btwSample = Math.max(-1, Math.min(1, btwChnls[btwIndex][btwOffset])) // clamp
+                  btwSample = (0.5 + btwSample < 0 ? btwSample * 32768 : btwSample * 32767) || 0 // scale to 16-bit signed int
+                  btwView.setInt16(btwPos, btwSample, true) // write 16-bit sample
+                  btwPos += 2
+                }
+                btwOffset++ // next source sample
+              }
+              const wavHdr = lamejs.WavHeader.readHeader(new DataView(btwArrBuff))
+              const data = new Int16Array(btwArrBuff, wavHdr.dataOffset, wavHdr.dataLen / 2)
+              // wav to MP3 (only channels 1)
+              const channels = wavHdr.channels
+              const sampleRate = wavHdr.sampleRate
+              const buffer = []
+              const mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128)
+              let remaining = data.length
+              const samplesPerFrame = 1152
+              let mp3buf
+              for (let i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
+                const mono = data.subarray(i, i + samplesPerFrame)
+                mp3buf = mp3enc.encodeBuffer(mono)
+                if (mp3buf.length > 0) {
+                  buffer.push(mp3buf)// new Int8Array(mp3buf));
+                }
+                remaining -= samplesPerFrame
+              }
+              const d = mp3enc.flush()
+              if (d.length > 0) {
+                buffer.push(new Int8Array(d))
+              }
+              const mp3Blob = new Blob(buffer, {type: 'audio/mp3'})
+              resolve(mp3Blob)
+            } catch (error) {
+              console.log(error)
+            }
+          })
+        })
+      })
     }
   }
 }
